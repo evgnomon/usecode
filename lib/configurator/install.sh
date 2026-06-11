9b5755246d1e32a4442b68f288a7f93760aa3ea6
Hamed Ghasemzadeh <hg@evgnomon.org>
2026-06-11 18:47:19 +0200

    Ubuntu

--- lib/configurator/install.sh
+++ lib/configurator/install.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+IS_WSL2=$(grep -qi "microsoft" /proc/version 2>/dev/null && echo true || echo false)
+PYTHON_VERSION="3.14.3"
+
 sudo apt update && sudo apt install -y git
 sudo apt upgrade -y
 
@@ -19,15 +22,19 @@ export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init --path)"
 export PATH="$HOME/.local/bin:$HOME/go/bin:$PATH"
 
-sudo apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev curl libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev hwdata yubikey-manager scdaemon scdaemon ykcs11 libpcsclite-dev swig pcscd libpam-u2f pinentry-tty libpam-yubico usbutils unzip libyaml-dev
+sudo apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev curl libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev hwdata pinentry-tty usbutils unzip libyaml-dev
 
-sudo update-alternatives --install /usr/local/bin/usbip usbip $(command -v ls /usr/lib/linux-tools/*/usbip | tail -n1) 20
-curl -sSL https://raw.githubusercontent.com/Yubico/libfido2/main/udev/70-u2f.rules | sudo tee /etc/udev/rules.d/70-u2f.rules > /dev/null
+if [ "$IS_WSL2" != "true" ]; then
+  sudo apt install -y yubikey-manager scdaemon ykcs11 libpcsclite-dev swig pcscd libpam-u2f libpam-yubico
+  USBIP_BIN=$(ls /usr/lib/linux-tools/*/usbip 2>/dev/null | tail -n1)
+  [ -n "$USBIP_BIN" ] && sudo update-alternatives --install /usr/local/bin/usbip usbip "$USBIP_BIN" 20
+  curl -sSL https://raw.githubusercontent.com/Yubico/libfido2/main/udev/70-u2f.rules | sudo tee /etc/udev/rules.d/70-u2f.rules > /dev/null
+fi
 
 [ ! -d ~/.pyenv/versions/2.7.18 ] && PYTHON_CONFIGURE_OPTS="--enable-shared" pyenv install 2.7.18
-[ ! -d ~/.pyenv/versions/3.14.3 ] && PYTHON_CONFIGURE_OPTS="--enable-shared" pyenv install 3.14.3
+[ ! -d ~/.pyenv/versions/$PYTHON_VERSION ] && PYTHON_CONFIGURE_OPTS="--enable-shared" pyenv install $PYTHON_VERSION
 
-pyenv global 3.14.3 2.7.18
+pyenv global $PYTHON_VERSION 2.7.18
 pip install --upgrade pip
 pip install --upgrade ansible pyyaml
 
@@ -38,8 +45,9 @@ which python
 eval $(cat "$HOME/.cargo/env")
 [ ! -d $HOME/.local/bin ] && mkdir -p $HOME/.local/bin
 
-sudo mkdir -p /etc/polkit-1/rules.d
-echo '
+if [ "$IS_WSL2" != "true" ]; then
+  sudo mkdir -p /etc/polkit-1/rules.d
+  echo '
 polkit.addRule(function(action, subject) {
     if ((action.id == "org.debian.pcsc-lite.access_pcsc" || action.id == "org.debian.pcsc-lite.access_card" ) && subject.isInGroup("plugdev")) {
         return polkit.Result.YES;
@@ -47,6 +55,7 @@ polkit.addRule(function(action, subject) {
 
 });
 ' | sudo tee /etc/polkit-1/rules.d/90-pcscd.rule > /dev/null
+fi
 
 if [ ! -d $HOME/src/github.com/evgnomon ]; then
         mkdir -p $HOME/src/github.com/evgnomon
@@ -60,7 +69,11 @@ fi
 
 cd $HOME/src/github.com/evgnomon/usecode/lib/configurator
 
-PLAYARGS=""
+PLAYARGS="-e python_version=$PYTHON_VERSION"
+
+if [ "$IS_WSL2" = "true" ]; then
+  PLAYARGS="$PLAYARGS -e wsl2=true"
+fi
 
 if [ ! -z "$DEV_CONTAINER" ]; then
   PLAYARGS="$PLAYARGS -e dev_container=true"
