3c052b8039c676dbd5c03bfdc548216ac0a62c8b
Hamed Ghasemzadeh <hg@evgnomon.org>
2026-06-13 12:46:53 +0200

    pull in case of existing

--- play.sh
+++ play.sh
@@ -10,6 +10,9 @@ cd ~/src/github.com/evgnomon
 if [ ! -d "usecode" ]; then
   git clone https://github.com/evgnomon/usecode.git
   git checkout ubuntu
+else
+  git pull
+  git checkout ubuntu
 fi
 
 cd usecode/lib/configurator
